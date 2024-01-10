b71b9a2712b1f3ca64f8a7768073689e1d6675a8
vishalgupta <vishal.gupt0407@gmail.com>
2024-01-10 21:03:02 +0530

    Added FileStorageException file

---
+++ src/main/java/io/vishal/filedemo/exception/FileStorageException.java
@@ -0,0 +1,13 @@
+package io.vishal.filedemo.exception;
+
+public class FileStorageException extends RuntimeException {
+
+    public FileStorageException() {
+    }
+
+    public FileStorageException(String message) {
+        super(message);
+    }
+}
+
+
