0b8e10588465ab88b56f9e67aae738789da624b4
vishalgupta <vishal.gupt0407@gmail.com>
2024-01-10 21:06:36 +0530

    Created FileUploadController and created PostMapping to upload file.

--- src/main/java/io/vishal/filedemo/controller/FileUploadController.java
+++ src/main/java/io/vishal/filedemo/controller/FileUploadController.java
@@ -1,7 +1,18 @@
 package io.vishal.filedemo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import io.vishal.filedemo.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class FileUploadController {
+
+    @Autowired
+    private StorageService storageService;
+
+    @PostMapping("/uploadFile")
+    public void uploadFile(@RequestParam("file")MultipartFile file) {
+        storageService.storeFile(file);
+    }
 }
